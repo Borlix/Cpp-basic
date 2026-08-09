3fd0ce22d67cb6f8937acbfe50f2cdae846d73e0
Borlix <309441135+Borlix@users.noreply.github.com>
2026-08-09 16:46:52 +0100

    Add files via upload

---
+++ C++/BasicSyntax.cpp
@@ -0,0 +1,19 @@
+// C++ basic syntax / boilerplate code. 
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string name = "peace";
+    int age = 100;
+    bool  is_student = true;
+    float money = 0.00 ;
+
+    cout<< name << endl ;
+    cout<< age << endl ;
+    cout<< is_student << endl ;
+    cout<< money << endl ;
+    return 0;
+}
+
+// there are int,float,doubal,char, string and bool are some data type's
